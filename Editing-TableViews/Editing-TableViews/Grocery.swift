358edd97301caee933f1f78c6e4017dbe916523c
Ameni Alsaydi <amenialsaydi@pursuit.org>
2019-11-21 22:31:17 -0500

    GroceryListController properly conforms to UITableViewDataSource

--- Editing-TableViews/Editing-TableViews/Grocery.swift
+++ Editing-TableViews/Editing-TableViews/Grocery.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+enum ItemStatus: String, CaseIterable  {
+    case purchased
+    case unpurchased
+}
+
+struct Grocery{
+    
+    var name: String
+    var price: Double
+    var status: ItemStatus
+    
+    static func createEmptyArray() -> [[Grocery]] {
+        let groceries = Array(repeating: [Grocery](), count: ItemStatus.allCases.count)
+        
+        return groceries
+    }
+    
+}
--- Editing-TableViews/Editing-TableViews/GroceryListController.swift
+++ Editing-TableViews/Editing-TableViews/GroceryListController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GroceryListController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var groceries = [[Grocery]]() {
+        didSet {
+            tableView.reloadData() // not sure if ill need this
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        groceries = Grocery.createEmptyArray()
+        tableView.dataSource = self
     }
 
 
+}
+
+extension GroceryListController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groceries[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        
+        
+        let grocery = groceries[indexPath.section][indexPath.row]
+        
+        cell.textLabel?.text = grocery.name
+        cell.detailTextLabel?.text = grocery.price.description
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return groceries.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        groceries[section].first?.status.rawValue
+    }
+    
 }
 
